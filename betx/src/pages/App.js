e633bb2e7747fad32a31e772d933f4a6c1ea8c0b
WIN-9M659CUL90U\Administrator <j.u.sophia@qq.com>
2018-11-25 00:08:46 +0800

    .

--- betx/src/pages/App.js
+++ betx/src/pages/App.js
@@ -1,40 +1,41 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router,Route,Switch,Redirect,NavLink} from 'react-router-dom';
 
+import Campaign from './campaign/campaign'
 import logo from '../assets/imgs/logo.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-        <Router>
+        <Router >
             <div className="App">
                 <header className="App-header">
                     <div className="header-nav">
-                        <img src={logo} alt=""/>
+                        <img className="logo" src={logo} alt=""/>
                         <ul className="nav-lists">
                             <li className="nav-item">
-                                <NavLink to="/" >
+                                <NavLink to="/index" >
                                     <span>首页</span>
                                 </NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink to="/" >
+                                <NavLink to="/game" >
                                     <span>游戏</span>
                                 </NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink to="/" >
+                                <NavLink to="/campaign" >
                                     <span>竞选庄家</span>
                                 </NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink to="/" >
+                                <NavLink to="/qa" >
                                     <span>常见问题</span>
                                 </NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink to="/" >
+                                <NavLink to="/paper" >
                                     <span>白皮书</span>
                                 </NavLink>
                             </li>
@@ -42,7 +43,7 @@ class App extends Component {
                         </ul>
                     </div>
                     <div className="header-control">
-                        <a href="/">登录</a>
+                        <a className="login" href="/">登录</a>
                         <div className="i18n">
                             <span>English</span>
                             <span className="current">中文</span>
@@ -51,7 +52,8 @@ class App extends Component {
                 </header>
                 <div className="App-view">
                     <Switch>
-                        <Route path="/"></Route>
+                        <Route path="/camppaign" component={Campaign}></Route>
+                        <Redirect from="/" to="camppaign" />
                     </Switch>
                 </div>
             </div>
--- betx/src/pages/campaign/campaign.js
+++ betx/src/pages/campaign/campaign.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import "../../style/css/campaign.css";
+import alarm from '../../assets/imgs/alarm.png';
+import EOS from '../../assets/imgs/EOS.png';
+import logoGrey from '../../assets/imgs/logo-grey.png';
 
 class Campaign extends React.Component {
     constructor() {
@@ -16,8 +19,74 @@ class Campaign extends React.Component {
 
         return (
             <div className="campaign">
+                <h3 className="page-title">超级庄家</h3>
+                <div className="banker section">
+                    <div className="real_time_data">
+                        <div className="data-block">
+                            <h5 className="data-title">本轮庄家实时数据</h5>
+                            <span>发起人：豌豆</span>
+                        </div>
+                        <div className="data-block">
+                            <h5 className="data-title">本轮庄家实时数据</h5>
+                            <div className="alarm">
+                                <img src={alarm} className="alarm_logo" alt=""/>
+                                <span>00,39,16</span>
+                            </div>
+                        </div>
+                        <div className="data-block">
+                            <h5 className="data-title">本轮累计收益</h5>
+                            <div className="data-inner-block">
+                                <div>
+                                    <div className="inner-icon">
+                                        <img src={EOS} alt=""/>
+                                        <span>EOS</span>
+                                    </div>
+                                    <span>0.1234</span>
+                                </div>
+                                <div>
+                                    <div className="inner-icon">
+                                        <img src={logoGrey} alt=""/>
+                                        <span>BETX</span>
+                                    </div>
+                                    <span>0.6666</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="data-block">
+                            <h5 className="data-title">拍卖价格</h5>
+                            <div className="price">
+                                <img src={logoGrey} className="price_logo" alt=""/>
+                                <span>1.0324</span>
+                            </div>
+                        </div>
 
+                    </div>
+                    <div className="income">
+                        <h5>我的超级庄家收入</h5>
+                        <div>
+                            <div className="data-inner-block">
+                                <div>
+                                    <div className="inner-icon">
+                                        <img src={EOS} alt=""/>
+                                        <span>EOS</span>
+                                    </div>
+                                    <span>0.1234</span>
+                                </div>
+                                <div>
+                                    <div className="inner-icon">
+                                        <img src={logoGrey} alt=""/>
+                                        <span>BETX</span>
+                                    </div>
+                                    <span>0.6666</span>
+                                </div>
+                            </div>
+                            <button className="draw_btn">领取</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
 }
+
+export default Campaign;
