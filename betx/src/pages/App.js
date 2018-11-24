ff69a42b58ca21e175a904dde3444ae17d294ff6
WIN-9M659CUL90U\Administrator <j.u.sophia@qq.com>
2018-11-24 22:42:13 +0800

    start the page

--- betx/src/pages/App.js
+++ betx/src/pages/App.js
@@ -1,26 +1,62 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+import {BrowserRouter as Router,Route,Switch,Redirect,NavLink} from 'react-router-dom';
+
+import logo from '../assets/imgs/logo.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-           hello world,I just forget the react
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Router>
+            <div className="App">
+                <header className="App-header">
+                    <div className="header-nav">
+                        <img src={logo} alt=""/>
+                        <ul className="nav-lists">
+                            <li className="nav-item">
+                                <NavLink to="/" >
+                                    <span>首页</span>
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/" >
+                                    <span>游戏</span>
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/" >
+                                    <span>竞选庄家</span>
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/" >
+                                    <span>常见问题</span>
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/" >
+                                    <span>白皮书</span>
+                                </NavLink>
+                            </li>
+
+                        </ul>
+                    </div>
+                    <div className="header-control">
+                        <a href="/">登录</a>
+                        <div className="i18n">
+                            <span>English</span>
+                            <span className="current">中文</span>
+                        </div>
+                    </div>
+                </header>
+                <div className="App-view">
+                    <Switch>
+                        <Route path="/"></Route>
+                    </Switch>
+                </div>
+            </div>
+        </Router>
+
     );
   }
 }
---
+++ betx/src/pages/campaign/campaign.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import "../../style/css/campaign.css";
+
+class Campaign extends React.Component {
+    constructor() {
+        super();
+        this.state={
+
+        }
+    }
+    componentDidMount() {
+
+    }
+    render() {
+
+        return (
+            <div className="campaign">
+
+            </div>
+        )
+    }
+}
